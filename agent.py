41cb4a279a87d7993c2f58644b83cf8c58f3cfb1
baixianger <58243749+baixianger@users.noreply.github.com>
2025-04-26 01:45:06 +0200

    add retriever tool

--- agent.py
+++ agent.py
@@ -1,15 +1,22 @@
 """LangGraph Agent"""
-import dotenv
-from langgraph.graph import MessagesState
-from langgraph.graph import START, StateGraph
+import os
+from dotenv import load_dotenv
+from langgraph.graph import START, StateGraph, MessagesState
 from langgraph.prebuilt import tools_condition
 from langgraph.prebuilt import ToolNode
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_groq import ChatGroq
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint, HuggingFaceEmbeddings
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain_community.document_loaders import WikipediaLoader
 from langchain_community.document_loaders import ArxivLoader
+from langchain_community.vectorstores import SupabaseVectorStore
+from langchain_core.messages import SystemMessage, HumanMessage
 from langchain_core.tools import tool
-from langchain_core.messages import SystemMessage
+from langchain.tools.retriever import create_retriever_tool
+from supabase.client import Client, create_client
+
+load_dotenv()
 
 @tool
 def multiply(a: int, b: int) -> int:
@@ -105,6 +112,25 @@ def arvix_search(query: str) -> str:
         ])
     return {"arvix_results": formatted_search_docs}
 
+# build a retriever tool
+embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-mpnet-base-v2") #  dim=768
+supabase: Client = create_client(
+    os.environ.get("SUPABASE_URL"), 
+    os.environ.get("SUPABASE_SERVICE_KEY"))
+vector_store = SupabaseVectorStore(
+    client=supabase,
+    embedding= embeddings,
+    table_name="documents",
+    query_name="match_documents_langchain",
+)
+question_retrieve_tool = create_retriever_tool(
+    vector_store.as_retriever(),
+    "Question Retriever",
+    "Find similar questions in the vector database for the given question.",
+)
+
+
+
 tools = [
     multiply,
     add,
@@ -114,13 +140,8 @@ tools = [
     wiki_search,
     web_search,
     arvix_search,
+    question_retrieve_tool
 ]
-
-
-# Load environment variables from .env file
-dotenv.load_dotenv()
-llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
-llm_with_tools = llm.bind_tools(tools)
 
 # load the system prompt from the file
 with open("system_prompt.txt", "r", encoding="utf-8") as f:
@@ -129,14 +150,35 @@ with open("system_prompt.txt", "r", encoding="utf-8") as f:
 # System message
 sys_msg = SystemMessage(content=system_prompt)
 
-# Node
-def assistant(state: MessagesState):
-    """Assistant node"""
-    return {"messages": [llm_with_tools.invoke([sys_msg] + state["messages"])]}
+
 
 # Build graph function
-def build_graph():
+def build_graph(provider: str = "groq"):
     """Build the graph"""
+    # Load environment variables from .env file
+    if provider == "google":
+        # Google Gemini
+        llm = ChatGoogleGenerativeAI(model="gemini-2.0-flash", temperature=0)
+    elif provider == "groq":
+        # Groq https://console.groq.com/docs/models
+        llm = ChatGroq(model="gemma2-9b-it", temperature=0) # optional : qwen-qwq-32b
+    elif provider == "huggingface":
+        # TODO: Add huggingface endpoint
+        llm = ChatHuggingFace(
+            llm=HuggingFaceEndpoint(
+                url="https://api-inference.huggingface.co/models/Meta-DeepLearning/llama-2-7b-chat-hf",
+                temperature=0,
+            ),
+        )
+    else:
+        raise ValueError("Invalid provider. Choose 'google', 'groq' or 'huggingface'.")
+    # Bind tools to LLM
+    llm_with_tools = llm.bind_tools(tools)
+
+    # Node
+    def assistant(state: MessagesState):
+        """Assistant node"""
+        return {"messages": [llm_with_tools.invoke([sys_msg] + state["messages"])]}
 
     builder = StateGraph(MessagesState)
     builder.add_node("assistant", assistant)
@@ -150,3 +192,16 @@ def build_graph():
 
     # Compile graph
     return builder.compile()
+
+# test
+if __name__ == "__main__":
+    question = "Who nominated the only Featured Article on English Wikipedia about a dinosaur that was promoted in November 2016?"
+
+    # Build the graph
+    graph = build_graph(provider="groq")
+    # Run the graph
+    messages = [HumanMessage(content=question)]
+    messages = graph.invoke({"messages": messages})
+    answer = messages[-1].content
+    print(f"Question: {question}")
+    print(f"{answer}")
